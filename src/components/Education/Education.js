e266129d9e5b60b154585bd12f1359fb5be05ac7
Engin Sasmaz <engins.93@gmail.com>
2019-12-27 17:58:10 +0100

    added additional educations

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -5,19 +5,23 @@ export default {
         {
           schoolName: 'The Hague University of Applied Sciences',
           study: 'Communication & Multimedia Design',
-          period: '2011 - 2016'
+          period: '2011 - 2016',
+          md: '12'
         },
         {
           schoolName: 'Aloysius College',
           study: 'Havo',
-          period: '2008 - 2011'
+          period: '2008 - 2011',
+          md: '6'
         },
         {
           schoolName: 'Mient College',
           study: 'Havo',
-          period: '2005 - 2008'
-        }
-      ]
+          period: '2005 - 2008',
+          md: '6'
+        },
+      ],
+      extras: ['Informatica', 'Game development', 'Internet Marketing & Management', 'Internet Marketing & Analytics', '3D editing']
     };
   }
 };
--- src/components/Education/Education.vue
+++ src/components/Education/Education.vue
@@ -7,17 +7,33 @@
         </v-col>
       </v-row>
       <v-row>
-        <v-col 
-          cols="12" 
-          sm="12" 
-          md="4"
-          v-for="(education, index) in educations" 
-          :key="`education-${index}`"
-          class="education"
-        >
-          <h3 class="schoolName">{{ education.schoolName }}</h3>
-          <div class="study">{{ education.study }}</div>
-          <div class="period">{{ education.period }}</div>
+        <v-col cols="12" sm="8">
+          <v-row>
+            <v-col 
+              cols="12" 
+              sm="12" 
+              :md="education.md"
+              v-for="(education, index) in educations" 
+              :key="`education-${index}`"
+              class="education"
+            >
+              <h3 class="schoolName">{{ education.schoolName }}</h3>
+              <div class="study">{{ education.study }}</div>
+              <div class="period">{{ education.period }}</div>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <v-row>
+            <v-col cols="12">
+              <h3 class="schoolName">Extra onderwijs</h3>
+              <div class="extras">
+                <ul v-for="(extra, index) in extras" :key="`extra-${index}`">
+                  <li>{{ extra }}</li>
+                </ul>
+              </div>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>  
