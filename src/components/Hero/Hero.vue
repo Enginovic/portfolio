3e6c3604f4510fd78573d0ef6a3210206e03c472
Engin Sasmaz <engins.93@gmail.com>
2019-12-28 01:13:27 +0100

    parallax => normal div with styling fixes

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -1,17 +1,16 @@
 <template>
-  <v-parallax
-    dark
-    src="@/assets/images/hero.jpg"
-  >
-    <v-row align="center" justify="center">
-      <v-col class="text-center hero" cols="12">
-        <img src="@/assets/images/engin.jpg" class="hero__image" />
-        <h1 class="display-1 font-weight-thin mb-4">Engin Sasmaz</h1>
-        <h4 class="subheading">Front End / Webdeveloper</h4>
-        <div class="hero__subtitle-2">Freelancer</div>
-      </v-col>
-    </v-row>
-  </v-parallax>
+  <div class="hero">
+    <v-container pt-12>
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <img src="@/assets/images/engin.jpg" class="hero__image" />
+          <h1 class="font-weight-thin mb-4">Engin Sasmaz</h1>
+          <h4>Front End / Webdeveloper</h4>
+          <div class="hero__subtitle">Freelancer</div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <style lang="scss" src="@/components/Hero/hero.scss"></style>
