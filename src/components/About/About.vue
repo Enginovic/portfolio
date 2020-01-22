d72f0baf1091f458f51345eb81dc59cc7c9d5350
Engin Sasmaz <engins.93@gmail.com>
2020-01-22 15:57:15 +0100

    Removed phone number from evil recruiters. If you're a recruiter and you've come this far to find my number, congrats! Now go away :)

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -17,7 +17,6 @@
             <li><span class="listLabel">Geboortedatum:</span>06 juli 1993</li>
             <li><span class="listLabel">Nationaliteit:</span>Nederlands</li>
             <li><span class="listLabel">Woonplaatst:</span>Leidschendam</li>
-            <li><span class="listLabel">Nummer:</span>06 14 52 52 00</li>
             <li><span class="listLabel">E-mail:</span><a href="mailto:info@enginsasmaz.nl" target="_blank">info@enginsasmaz.nl</a></li>
           </ul>
         </v-col>
