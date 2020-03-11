2b6f9395e2ae3134b7b92694d4dc37100fede8a0
Engin Sasmaz <engins.93@gmail.com>
2020-03-11 12:59:44 +0100

    update experience text

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -3,31 +3,38 @@ export default {
     return {
       experiences: [
         {
+          companyName: 'Capgemini',
+          function: 'Front End Developer',
+          period: 'maa. 2020 - heden',
+          activity: 'Bij Capgemini werk ik als Front End Developer bij diverse klanten. ',
+          link: 'https://www.capgemini.com/'
+        },
+        {
           companyName: 'Atabix',
           function: 'Front End Developer',
-          period: '2018 - Heden',
-          activity: 'Bij Atabix ben ik aan de slag gegaan als junior developer en uiteindelijk ben ik binnen het bedrijf  gegroeid tot medior developer. Ik mocht mij hier gaan verdiepen in VueJS en Typescript. In Vue heb ik projecten gerealiseerd in zowel groepsverband als zelfstandig in samenwerking met een backender. Zo heb ik meerdere dashboards mogen realiseren waarin een gebruiker een profiel pagina heeft en daar zijn gegevens kan wijzigen. Het tonen van data in verschillende vormen waren voornamelijk mijn taken binnen dit bedrijf. Daarnaast mocht ik huidige projecten onderhouden door nieuwe features te bouwen in VanillaJS en jQuery.',
+          period: 'mei 2018 - feb. 2020',
+          activity: 'Bij Atabix ben ik aan de slag gegaan als VueJS developer. Ik ben mij hier verder gaan verdiepen in VueJS en Typescript. Ik realiseerde in VueJS projecten in zowel groepsverband als zelfstandig in samenwerking met een backender. Zo realiseerde ik meerdere dashboards waarin een gebruiker een profiel pagina heeft en daar zijn gegevens kan wijzigen. Mijn taak binnen het bedrijf was het tonen van data in verschillende vormen, zoals in een tabel vorm of in een profiel pagina. Daarnaast onderhield ik huidige projecten door nieuwe features te bouwen in VanillaJS en jQuery.',
           link: 'https://atabix.com/'
         },
         {
           companyName: 'Strossle',
           function: 'Front End Developer',
-          period: '2016 - 2018',
-          activity: 'Bij Strossle ben ik begonnen als starter. Hier mocht ik widgets die onderaan een artikel van een pagina staan, maar ook op andere plekken van een pagina zaten, stylen. De widget bestond uit een 2-regel code die door de eigenaar van de website geplaatst moest worden. Wanneer dat was gedaan mocht ik de widget gaan stylen aan de hand van de huisstijl van de website, dit deed ik in CSS3. Daarnaast werkte ik met veel verschillende publishers en hielp ik ze meedenken met het verbeteren van de UX en UI van hun website. Tot slot pakte ik ook support tickets op als de publisher de code er niet correct in kreeg.',
+          period: 'feb. 2016 - apr. 2018',
+          activity: 'Bij Strossle stylde ik widgets die onderaan een artikel van een pagina staan en ook op andere plekken van een pagina. De widget was een 2 regel code die door de eigenaar van de website geplaatst moest worden en zodra dat was gedaan mocht ik de widget gaan stylen aan de hand van de huiststijl van de website. Dit deed ik in CSS3 met een JSON omgeving. Daarnaast werkte ik met veel verschillende publishers/uitgevers van diverse toonaangevende tijdschriften. Hier hielp ik met meedenken en het verbeteren van de UX en UI van hun website. Tot slot pakte ik ook support tickets op als de publisher de code er niet correct inkregen.',
           link: 'https://strossle.com/'
         },
         {
           companyName: 'Webbeat',
           function: 'Front End Developer & UX Designer',
-          period: '2015 - 2015',
-          activity: 'Bij Webbeat heb ik mijn afstudeerstage mogen uitvoeren. Ik moest hier een webapplicatie dat door Webbeat was gebouwd redesignen en in HTML5 en CSS3 een prototype van maken. Naast mijn afstudeerstage heb ik websites in Bootstrap gemaakt en opgeleverd aan de klanten, zonder hier een CMS aan te koppelen.',
+          period: 'mei 2015 - okt. 2015',
+          activity: 'Bij Webbeat heb ik mijn afstudeerstage uitgevoerd. Ik moest hier een webapplicatie dat door Webbeat was gebouwd redesignen en in HTML5 en CSS3 een prototype van maken. Naast mijn afstudeerstage heb ik websites in Bootstrap gemaakt en opgeleverd aan de klanten, zonder hier een CMS aan te koppelen.',
           link: 'https://webbeat.nl/'
         },
         {
           companyName: 'Snap Media',
           function: 'Web Developer',
-          period: '2014 - 2014',
-          activity: 'Bij Snapmedia heb ik mijn 3e-jaars stage mogen afronden. Hier heb ik websites tekstueel onderhouden, templates in Wordpress en Joomlas gemaakt en nieuwe ontwerpen gemaakt van websites in Photoshop of Illustrator.'
+          period: 'feb. 2014 - jun. 2014',
+          activity: 'Bij Snapmedia heb ik mijn 3e-jaars stage afgeronden. Hier heb ik websites tekstueel onderhouden, templates in Wordpress en Joomlas gemaakt en nieuwe ontwerpen gemaakt van websites in Photoshop of Illustrator.'
         }
       ]
     };
