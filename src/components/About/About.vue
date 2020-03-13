5a9c77b93d3565b744e5f2af5155f42edc4be410
Engin Sasmaz <engins.93@gmail.com>
2020-03-13 13:53:51 +0100

    removed cv temp.

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -25,7 +25,7 @@
           <p>Leergierig, simplistisch en een doorzetter zijn de eigenschappen die mij een prettige collega maken.</p>
           <p>Ik maak mijn projecten met de nodige zorg en van optimale kwaliteit. Door het nakomen van afspraken houd ik mij altijd aan de gestelde budgetten. Wanneer ik niet de enige developer in de kamer ben stel ik mijzelf coachbaar op, maar deel ik ook nodige inhoudelijke kennis en ervaring.</p>
           <p>Tot slot stimuleer ik een positieve houding onder collega's.</p>
-          <v-btn large class="button button--secondary"><a class="white--text" href="/CV_Engin_Sasmaz.jpg" target="_blank">Download CV</a></v-btn>
+          <!-- <v-btn large class="button button--secondary"><a class="white--text" href="/CV_Engin_Sasmaz.jpg" target="_blank">Download CV</a></v-btn> -->
         </v-col>
       </v-row>
     </v-container>  
